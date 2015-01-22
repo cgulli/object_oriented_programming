b9be06b6e78d91dcc0ee2ea645838481726c693e
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-22 13:53:37 -0500

    added parent class

--- people.rb
+++ people.rb
@@ -1,12 +1,26 @@
-class Students
+
+
+
+class Person 
+	def initialize(name)
+		@name = name
+	end
+	def to_s
+		"Hi my name is #{@name}"
+	end
+end
+
+class Students < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby in an Object"
 	end
 end
 
+student = Students.new("Chris")
+puts student.to_s
